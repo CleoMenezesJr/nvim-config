f8060202f4b86320420384af21cd35a707e6b7fc
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-06-02 23:11:07 -0400

    perf(lsp): make format-on-save async to avoid blocking the editor

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -77,7 +77,7 @@ vim.api.nvim_create_augroup('AutoFormatting', {})
 vim.api.nvim_create_autocmd('BufWritePre', {
   group = 'AutoFormatting',
   callback = function()
-    vim.lsp.buf.format()
+    vim.lsp.buf.format({ async = true })
   end,
 })
 

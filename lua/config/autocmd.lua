49993cb86ac9d4aa8c8c38df5b37d0feb1d9eabc
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-06-02 23:12:12 -0400

    fix(autocmd): remove duplicate BufReadPost cursor-restore handler
    The first handler (with zz centering) is correct. The second was shadowing
    it without the scroll-centering and without clear=true on the augroup.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -81,19 +81,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
-
--- Return to last edit position when opening files
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
+local augroup = vim.api.nvim_create_augroup("UserConfig", { clear = true })
 
 -- Auto-close terminal when process exits
 vim.api.nvim_create_autocmd("TermClose", {

b15a6d124ffc7d414b6fb792e3a82f39e5dec4c6
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-08-19 14:21:14 -0400

    chore(lsp): move lsp auto completion to auto cmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -160,6 +160,17 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+
+-- LSP auto completion
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client ~= nil and client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
 -- showing lsp progress with nvim_echo and LspProgress
 -- vim.api.nvim_create_autocmd("LspProgress", {
 --   callback = function(ev)

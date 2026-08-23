e9715eceaf8fa22c3daf8b71ba1ee24a630aec6d
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-08-23 11:51:06 -0400

    chore(autocmd): fire format only for the attached lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -66,12 +66,17 @@ vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
 })
 
 -- Format on save
-vim.api.nvim_create_augroup('AutoFormatting', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  group = 'AutoFormatting',
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      group = vim.api.nvim_create_augroup('AutoFormatting', {}),
+      callback = function()
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
+      end,
+    })
+  end
 })
 
 local augroup = vim.api.nvim_create_augroup("UserConfig", { clear = true })

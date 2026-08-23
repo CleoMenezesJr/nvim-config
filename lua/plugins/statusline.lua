ca5f03ce22de8e19a5722bf417732f9f95174079
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-08-23 18:56:18 -0400

    chore(statusline): make lsp progress variable local instead of global

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -21,14 +21,14 @@ local modes = {
 }
 
 
-Lsp_progress = ""
+local lsp_progress = ""
 vim.api.nvim_create_autocmd("LspProgress", {
   callback = function(ev)
     local value = ev.data.params.value
     if value.kind ~= "end" then
-      Lsp_progress = "%#StlWorkspace#  " .. value.title .. " " .. value.percentage .. "%%" .. " %*"
+      lsp_progress = "%#StlWorkspace#  " .. value.title .. " " .. value.percentage .. "%%" .. " %*"
     else
-      Lsp_progress = ""
+      lsp_progress = ""
     end
     vim.cmd.redrawstatus()
   end,
@@ -73,7 +73,7 @@ function _G._statusline()
       mode ..
       " %*" ..
       get_git() ..
-      path .. modified_icon .. Lsp_progress .. "%=" .. diag .. "  " .. filetype .. "  " .. "%l:%c"
+      path .. modified_icon .. lsp_progress .. "%=" .. diag .. "  " .. filetype .. "  " .. "%l:%c"
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {

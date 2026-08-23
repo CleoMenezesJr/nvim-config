e4b94bf36c7059bf23dc3e9755147f95afe6d965
Cleo Menezes Jr. <cleomenezesjr@gmail.com>
2026-08-23 11:47:27 -0400

    chore(autocmd): remove junk code

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -159,19 +159,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end)
   end,
 })
-
--- showing lsp progress with nvim_echo and LspProgress
--- vim.api.nvim_create_autocmd("LspProgress", {
---   callback = function(ev)
---     vim.print(ev.data)
---     local value = ev.data.params.value
---     vim.api.nvim_echo({ { value.message or "done" } }, false, {
---       id = "lsp." .. ev.data.client_id,
---       kind = "progress",
---       source = "vim.lsp",
---       title = value.title,
---       status = value.kind ~= "end" and "running" or "success",
---       percent = value.percentage,
---     })
---   end,
--- })
